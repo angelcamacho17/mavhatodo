6e140d8a609d4200e1ca51e270646b88d9a09d73
angelcamacho17 <angelcamacho1708@gmail.com>
2019-10-04 15:38:38 -0300

    adding server todo

---
+++ todo-server/src/main/java/com/mavha/todolist/exception/FileStorageException.java
@@ -0,0 +1,11 @@
+package com.mavha.todolist.exception;
+
+public class FileStorageException extends RuntimeException {
+    public FileStorageException(String message) {
+        super(message);
+    }
+
+    public FileStorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ todo-server/src/main/java/com/mavha/todolist/model/Todo.java
@@ -0,0 +1,58 @@
+package com.mavha.todolist.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "todos")
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(max = 140)
+    private String description;
+
+    private Boolean status;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "file_id", nullable = false)
+    private DBFile file;
+
+    public DBFile getFile() {
+        return file;
+    }
+
+    public void setFile(DBFile file) {
+        this.file = file;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+
+    //private DBFIle file;
+
+}
